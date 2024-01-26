aa88e2ab95bfc57c010ad17ef6e77e0d5518825b
Kavindu Dilshan <kavindudilshan84@gmail.com>
2024-01-26 20:39:54 +0530

    complete calculator UI

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onEqualClick(view: View) {}
+    fun onDigitClick(view: View) {}
+    fun onAllClearClick(view: View) {}
+    fun onOperatorClick(view: View) {}
+    fun onBackClick(view: View) {}
+    fun onClearClick(view: View) {}
 }
